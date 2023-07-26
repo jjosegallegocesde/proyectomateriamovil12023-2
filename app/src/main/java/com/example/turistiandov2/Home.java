254a8abcb15879580420ca9895039364de450839
Bernardo A Mira García <bamira1@misena.edu.co>
2023-07-25 19:40:16 -0500

    ok

--- app/src/main/java/com/example/turistiandov2/Home.java
+++ app/src/main/java/com/example/turistiandov2/Home.java
@@ -3,6 +3,7 @@ package com.example.turistiandov2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,6 +11,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class Home extends AppCompatActivity {
 
@@ -50,6 +53,21 @@ public class Home extends AppCompatActivity {
     }
 
 
+    public void cambiarIdioma(String idioma){
+
+        //configurar el lenguaje del telefono
+        Locale lenguaje=new Locale(idioma);
+        Locale.setDefault(lenguaje);
+
+        //Configuramos globalmente el telefono
+        Configuration configuracionTelefono=getResources().getConfiguration();
+        configuracionTelefono.locale=lenguaje;
+
+        //Ejecutamos la configuracion
+        getBaseContext().getResources().updateConfiguration(configuracionTelefono,getBaseContext().getResources().getDisplayMetrics());
+
+    }
+
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu,menu);
         return true;
@@ -60,10 +78,14 @@ public class Home extends AppCompatActivity {
         switch (itemSeleccionado){
 
             case(R.id.opcion1):
-
+                this.cambiarIdioma("en");
+                Intent intentIngles=new Intent(Home.this,Home.class);
+                startActivity(intentIngles);
                 break;
             case(R.id.opcion2):
-
+                this.cambiarIdioma("es");
+                Intent intentEspanol=new Intent(Home.this,Home.class);
+                startActivity(intentEspanol);
                 break;
             case(R.id.opcion3):
 
@@ -80,8 +102,6 @@ public class Home extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void cambiarIdioma(String idioma){
 
-    }
 
 }
